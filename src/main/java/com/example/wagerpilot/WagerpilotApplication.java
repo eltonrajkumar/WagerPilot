66b82b98bb575568c12f503aaa792bfa031a652e
eltonrajkumar <eltonrajkumar@gmail.com>
2026-07-06 23:24:02 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/example/wagerpilot/WagerpilotApplication.java
@@ -0,0 +1,13 @@
+package com.example.wagerpilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WagerpilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WagerpilotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wagerpilot/WagerpilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wagerpilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WagerpilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
