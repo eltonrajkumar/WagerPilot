00bbefa451861a44a0320f941b6b620c3dbe8c84
eltonrajkumar <eltonrajkumar@gmail.com>
2026-07-09 22:29:06 +0530

    Completed User Entity

--- src/main/java/com/elton/wagerpilot/entity/User.java
+++ src/main/java/com/elton/wagerpilot/entity/User.java
@@ -1,12 +1,34 @@
 package com.elton.wagerpilot.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "users")
-
+@Getter
+@Setter
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, unique = true , length = 50)
+    private String username;
+
+    @Column(nullable = false,unique = true,length = 100)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+
+
+
+
 }
