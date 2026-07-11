d6e4d1c792ee103d3afd693e3660f3c5c5e8832b
eltonrajkumar <eltonrajkumar@gmail.com>
2026-07-11 17:51:51 +0530

    Implemented User Registration API

--- src/main/java/com/elton/wagerpilot/controller/UserController.java
+++ src/main/java/com/elton/wagerpilot/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.elton.wagerpilot.controller;
 
 
+import com.elton.wagerpilot.dto.RegisterRequest;
 import com.elton.wagerpilot.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +19,9 @@ public class UserController {
 @GetMapping("/test")
 public String test(){
         return userService.printMessage();
+}
+@PostMapping("/register")
+    public String register(@RequestBody RegisterRequest request){
+        return userService.registerUser(request);
 }
 }
--- src/main/java/com/elton/wagerpilot/service/UserService.java
+++ src/main/java/com/elton/wagerpilot/service/UserService.java
@@ -1,7 +1,11 @@
 package com.elton.wagerpilot.service;
 
+import com.elton.wagerpilot.dto.RegisterRequest;
+import com.elton.wagerpilot.entity.User;
 import com.elton.wagerpilot.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class UserService {
@@ -12,6 +16,29 @@ public class UserService {
     }
 
 public String  printMessage(){
-       return "Our service is working!!";
+    return "HELLO 12345";
 }
+    public String registerUser(RegisterRequest request) {
+
+        try {
+
+            User user = new User();
+
+            user.setUsername(request.getUsername());
+            user.setEmail(request.getEmail());
+            user.setPassword(request.getPassword());
+            user.setCreatedAt(LocalDateTime.now());
+
+            userRepository.save(user);
+
+            return "User Registered Successfully";
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+            return e.getClass().getName() + " : " + e.getMessage();
+
+        }
+    }
 }
